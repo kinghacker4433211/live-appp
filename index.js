0c3ebc7fc2161102a48cb6b34901517d3e543f4c
kinghacker4433211 <54596941+kinghacker4433211@users.noreply.github.com>
2020-09-06 10:00:42 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const axios = require("axios")
 const urls = ["https://hyper-branched-college.glitch.me"]
+const urls = ["https://kinghac.glitch.me"]
+
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
